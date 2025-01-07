46d554563128ed05c4b85f0c574f5cd668d289d3
saifbutt97 <78349354+saifbutt97@users.noreply.github.com>
2025-01-07 19:11:48 +0500

    sign data updated

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,40 +1,51 @@
-import { BaseEntity, Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
-import * as bcrypt from "bcrypt";
-import { UserRoles } from "../user-roles.enum";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserRoles } from '../user-roles.enum';
 
 @Entity({ name: 'users' })
 @Unique(['email', 'deletedAt'])
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-    @Column()
-    name: string;
-    @Column()
-    email: string;
-    @Column({ select: false })
-    password: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column({ select: false })
+  password: string;
+  @Column({ select: false })
+  carName: string;
+  @Column({ select: false })
+  carNumber: string;
+  @Column({ select: false })
+  salt: string;
+  @Column({
+    type: 'enum',
+    enum: UserRoles,
+    default: UserRoles.USER,
+  })
+  role: UserRoles;
 
-    @Column({ select: false })
-    salt: string;
+  @CreateDateColumn()
+  createdAt!: Date;
 
-    @Column({
-        type: 'enum',
-        enum: UserRoles,
-        default: UserRoles.USER
-    })
-    role: UserRoles;
+  @UpdateDateColumn()
+  updatedAt!: Date;
 
-    @CreateDateColumn()
-    createdAt!: Date;
+  @DeleteDateColumn()
+  deletedAt?: Date;
 
-    @UpdateDateColumn()
-    updatedAt!: Date;
-
-    @DeleteDateColumn()
-    deletedAt?: Date;
-
-    async validatePassword(password: string): Promise<boolean> {
-        const hash = await bcrypt.hash(password, this.salt);
-        return hash === this.password;
-    }
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
